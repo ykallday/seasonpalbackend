7ec85ff1b0f9cb3493374618f37f91d535271cbe
Yael Kaufman <ylkauf@gmail.com>
2023-04-07 16:18:04 -0700

    fixed admin

--- seasonpal/admin.py
+++ seasonpal/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, SeasonLocation, Produce, Note, Suggestion,Resource
 class CustomUserAdmin(admin.ModelAdmin):
     model = CustomUser
 # Register your models here.
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(SeasonLocation, Produce, Note, Suggestion, Resource)
